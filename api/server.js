b8426b187eaf0b08cef8d7dca5c7efb967b9f526
bukit3point0 <bukit3point0@gmail.com>
2021-05-07 03:15:56 -0400

    index, server pages created

--- api/server.js
+++ api/server.js
@@ -6,4 +6,15 @@ const server = express();
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 
+const actionRouter = require('./actions/actions-router')
+const projectRouter = require('./projects/projects-router')
+
+server.use(express.json())
+server.use('/api/actions', actionRouter)
+server.use('/api/projects', projectRouter)
+
+server.get('/', (req, res) => {
+    res.send(`<h2>API running</h2>`)
+})
+
 module.exports = server;
